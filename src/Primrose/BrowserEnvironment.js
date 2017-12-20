4b3e5d795a87cb016e88e5c9c06ebf8901ef52d2
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-12-20 11:09:55 -0500

    this is called "self-documenting code"

--- src/Primrose/BrowserEnvironment.js
+++ src/Primrose/BrowserEnvironment.js
@@ -12,90 +12,98 @@ import { Engine } from "./Physics";
 import { Teleporter } from "./Tools";
 import { Manager } from "./Network";
 
+function normalizeOptions(options) {
+
+  // normalize the inputs
+  options = coalesce({
+    backgroundColor: 0x000000,
+    enableShadows: true,
+    font: HELVETIKER,
+    fullScreenElement: document.body,
+    nonstandardNeckLength: 0.15,
+    nonstandardNeckDepth: 0.075,
+    plugins: [],
+    useFog: true,
+    useGaze: isCardboard
+  }, options);
+
+  if(!options.groundTexture && !options.groundModel) {
+    options.groundTexture = DECK_GRID;
+  }
+
+  if(!options.skyTexture) {
+    options.skyTexture = options.backgroundColor;
+  }
+
+
+  // configure default plugins
+  const add = (Class, opts) => {
+    if(!any(options.plugins, p => p instanceof Class)) {
+      options.plugins.push(new Class(opts));
+    }
+  };
+
+  add(iOSOrientationHack);
+
+  add(Audio3D, { ambientSound: options.ambientSound });
+
+  add(Music, { numNotes: options.numNotes });
+
+  add(Text3D, { font: options.font });
+
+  add(Sky, { texture: options.skyTexture });
+
+  add(Ground, {
+    texture: options.groundTexture,
+    model: options.groundModel
+  });
+
+  add(Fader, { rate: options.fadeRate });
+
+  add(Teleporter);
+
+  add(Engine, { gravity: options.gravity });
+
+  if(!options.disableKeyboard) {
+    add(Clipboard);
+  }
+
+  if(!options.disableGamepad && GamepadManager.isAvailable) {
+    add(GamepadManager);
+  }
+
+  if(options.enableShadows) {
+    add(Shadows, {
+      mapSize: options.shadowMapSize,
+      radius: options.shadowRadius,
+      cameraSize: options.shadowCameraSize
+    });
+  }
+
+  if(options.useFog) {
+    add(Fog);
+  }
+
+  if(options.avatarModel) {
+    add(ModelFactoryPlugin, {
+      name: "avatar",
+      modelFile: options.avatarModel
+    });
+
+    add(Manager);
+  }
+
+  return options;
+}
+
 export default class BrowserEnvironment extends Environment {
   constructor(options) {
 
-    const add = (Class, opts) => {
-      if(!any(options.plugins, p => p instanceof Class)) {
-        options.plugins.push(new Class(opts));
-      }
-    };
-
-    options = coalesce({
-      backgroundColor: 0x000000,
-      enableShadows: true,
-      font: HELVETIKER,
-      fullScreenElement: document.body,
-      nonstandardNeckLength: 0.15,
-      nonstandardNeckDepth: 0.075,
-      plugins: [],
-      useFog: true,
-      useGaze: isCardboard
-    }, options);
-
-    if(!options.groundTexture && !options.groundModel) {
-      options.groundTexture = DECK_GRID;
-    }
-
-    if(!options.skyTexture) {
-      options.skyTexture = options.backgroundColor;
-    }
-
-    add(iOSOrientationHack);
-
-    add(Audio3D, { ambientSound: options.ambientSound });
-
-    add(Music, { numNotes: options.numNotes });
-
-    add(Text3D, { font: options.font });
-
-    add(Sky, { texture: options.skyTexture });
-
-    add(Ground, {
-      texture: options.groundTexture,
-      model: options.groundModel
-    });
-
-    add(Fader, { rate: options.fadeRate });
-
-    add(Teleporter);
-
-    add(Engine, { gravity: options.gravity });
-
-    if(!options.disableKeyboard) {
-      add(Clipboard);
-    }
-
-    if(!options.disableGamepad && GamepadManager.isAvailable) {
-      add(GamepadManager);
-    }
-
-    if(options.enableShadows) {
-      add(Shadows, {
-        mapSize: options.shadowMapSize,
-        radius: options.shadowRadius,
-        cameraSize: options.shadowCameraSize
-      });
-    }
-
-    if(options.useFog) {
-      add(Fog);
-    }
-
-    if(options.avatarModel) {
-      add(ModelFactoryPlugin, {
-        name: "avatar",
-        modelFile: options.avatarModel
-      });
-
-      add(Manager);
-    }
-
-    super(options);
+    super(normalizeOptions(options));
 
     this.ready.then(() => {
-      if(options.fullScreenButtonContainer){
-        this.insertFullScreenButtons(options.fullScreenButtonContainer);
+      if(this.options.fullScreenButtonContainer){
+        this.insertFullScreenButtons(this.options.fullScreenButtonContainer);
       }
 
       window.addEventListener("vrdisplaypresentchange", (evt) => {
