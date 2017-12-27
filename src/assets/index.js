43b81c7e5eb50af1c5c48c1ac142c24aa1673265
Sean T. McBeth <smcbeth@deloitte.com>
2017-12-27 01:27:46 -0500

    move assets to their own folder

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+/*
+pliny.namespace({
+  name: "assets",
+  description: "A collection of source-inlined resources, for convenience"
+});
+*/
+
+import DeckGrid from "./DeckGrid";
+import Helvetiker from "./Helvetiker";
+
+export {
+  DeckGrid,
+  Helvetiker
+};
+
+export default {
+  DeckGrid,
+  Helvetiker
+};
