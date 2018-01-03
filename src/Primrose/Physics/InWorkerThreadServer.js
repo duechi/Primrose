038aac348cdc18b54d841efe1c0d9f9c9b632256
Sean T. McBeth <smcbeth@deloitte.com>
2018-01-02 21:37:01 -0500

    don't use the `arguments` property in hot functions

--- src/Primrose/Physics/InWorkerThreadServer.js
+++ src/Primrose/Physics/InWorkerThreadServer.js
@@ -4,12 +4,6 @@ import RPCBuffer from "./RPCBuffer";
 
 const rpc = new RPCBuffer();
 
-function pq() {
-  for(let i = 0; i < arguments.length; ++i) {
-    rpc.add(arguments[i]);
-  }
-}
-
 export default class InWorkerThreadServer extends BaseServerPlugin {
   constructor(options) {
     super(options);
@@ -82,62 +76,100 @@ export default class InWorkerThreadServer extends BaseServerPlugin {
   }
 
   setGravity(v) {
-    pq(CommandIDs["setGravity"], v);
+    rpc.add(CommandIDs["setGravity"]);
+    rpc.add(v);
   }
 
   enableAllowSleep() {
-    pq(CommandIDs["enableAllowSleep"]);
+    rpc.add(CommandIDs["enableAllowSleep"]);
   }
 
   disableAllowSleep() {
-    pq(CommandIDs["disableAllowSleep"]);
+    rpc.add(CommandIDs["disableAllowSleep"]);
   }
 
   newBody(id, mass, type) {
-    pq(CommandIDs["newBody"], id, mass, type);
+    rpc.add(CommandIDs["newBody"]);
+    rpc.add(id);
+    rpc.add(mass);
+    rpc.add(type);
   }
 
   removeBody(id) {
-    pq(CommandIDs["removeBody"], id);
+    rpc.add(CommandIDs["removeBody"]);
+    rpc.add(id);
   }
 
   addSphere(id, radius) {
-    pq(CommandIDs["addSphere"], id, radius);
+    rpc.add(CommandIDs["addSphere"]);
+    rpc.add(id);
+    rpc.add(radius);
   }
 
   addBox(id, width, height, depth) {
-    pq(CommandIDs["addBox"], id, width, height, depth);
+    rpc.add(CommandIDs["addBox"]);
+    rpc.add(id);
+    rpc.add(width);
+    rpc.add(height);
+    rpc.add(depth);
   }
 
   addPlane(id) {
-    pq(CommandIDs["addPlane"], id);
+    rpc.add(CommandIDs["addPlane"]);
+    rpc.add(id);
   }
 
   setPosition(id, x, y, z) {
-    pq(CommandIDs["setPosition"], id, x, y, z);
+    rpc.add(CommandIDs["setPosition"]);
+    rpc.add(id);
+    rpc.add(x);
+    rpc.add(y);
+    rpc.add(z);
   }
 
   setQuaternion(id, x, y, z, w) {
-    pq(CommandIDs["setQuaternion"], id, x, y, z, w);
+    rpc.add(CommandIDs["setQuaternion"]);
+    rpc.add(id);
+    rpc.add(x);
+    rpc.add(y);
+    rpc.add(z);
+    rpc.add(w);
   }
 
   setVelocity(id, x, y, z) {
-    pq(CommandIDs["setVelocity"], id, x, y, z);
+    rpc.add(CommandIDs["setVelocity"]);
+    rpc.add(id);
+    rpc.add(x);
+    rpc.add(y);
+    rpc.add(z);
   }
 
   setAngularVelocity(id, x, y, z) {
-    pq(CommandIDs["setAngularVelocity"], id, x, y, z);
+    rpc.add(CommandIDs["setAngularVelocity"]);
+    rpc.add(id);
+    rpc.add(x);
+    rpc.add(y);
+    rpc.add(z);
   }
 
   setLinearDamping(id, v) {
-    pq(CommandIDs["setLinearDamping"], id, v);
+    rpc.add(CommandIDs["setLinearDamping"]);
+    rpc.add(id);
+    rpc.add(v);
   }
 
   setAngularDamping(id, v) {
-    pq(CommandIDs["setAngularDamping"], id, v);
+    rpc.add(CommandIDs["setAngularDamping"]);
+    rpc.add(id);
+    rpc.add(v);
   }
 
   addSpring(id1, id2, restLength, stiffness, damping) {
-    pq(CommandIDs["addSpring"], id1, id2, restLength, stiffness, damping);
+    rpc.add(CommandIDs["addSpring"]);
+    rpc.add(id1);
+    rpc.add(id2);
+    rpc.add(restLength);
+    rpc.add(stiffness);
+    rpc.add(damping);
   }
 }
